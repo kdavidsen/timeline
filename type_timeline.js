19c2ab31ac8628256316c51af3341bcffed5112c
kdavidsen <david532@umn.edu>
2017-12-11 17:55:50 -0600

    timeline

---
+++ type_timeline.js
@@ -0,0 +1,130 @@
+$(document).ready(function(){
+
+	$(".event1").click(function(){
+
+    $(".description1").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event2").click(function(){
+
+    $(".description2").fadeToggle()
+    $(".description1").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+
+	$(".event3").click(function(){
+
+    $(".description3").fadeToggle()
+    $(".description2").hide();
+    $(".description1").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event4").click(function(){
+
+    $(".description4").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description1").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event5").click(function(){
+
+    $(".description5").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description1").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event6").click(function(){
+
+    $(".description6").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description1").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event7").click(function(){
+
+    $(".description7").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description1").hide();
+    $(".description8").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event8").click(function(){
+
+    $(".description8").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description1").hide();
+    $(".description9").hide();
+
+	}); 
+
+	$(".event9").click(function(){
+
+    $(".description9").fadeToggle()
+    $(".description2").hide();
+    $(".description3").hide();
+    $(".description4").hide();
+    $(".description5").hide();
+    $(".description6").hide();
+    $(".description7").hide();
+    $(".description8").hide();
+    $(".description1").hide();
+
+	}); 
+
+}); 
